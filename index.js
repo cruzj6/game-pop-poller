0abbe5e93b0df08d155240197c4276794af1c52d
josephcr <cruzjoey94@gmail.com>
2018-03-16 14:51:55 -0400

    bind to a port for heroku

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const logger = require('./common/logger');
 const twitchPolling = require('./apiWrappers/twitch/polling');
+const http = require('http');
 
 try {
 	// Init kafka messaging then begin polling
@@ -10,3 +11,5 @@ try {
 } catch (err) {
 	logger.error('Top level caught error: ', err);
 }
+
+http.createServer().listen(process.env.PORT || 8080);
